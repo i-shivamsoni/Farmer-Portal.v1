105af725ac00a34b3752f6e5f9dff4292e2af7f3
mohit-codes <kushwahamohit000@gmail.com>
2020-01-26 12:17:09 +0530

    Changes for multilingual feature

--- app/src/main/java/com/example/farmerportal/LangActivity.java
+++ app/src/main/java/com/example/farmerportal/LangActivity.java
@@ -1,48 +1,98 @@
 package com.example.farmerportal;
 
-import androidx.appcompat.app.AlertDialog;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActionBar;
-import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class LangActivity extends AppCompatActivity {
+    Locale myLocale;
+    Spinner spinner;
+    String currentLanguage = "en", currentLang;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lang);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        currentLanguage = getIntent().getStringExtra(currentLang);
+        spinner = (Spinner) findViewById(R.id.spinner2);
+        List<String> list = new ArrayList<>();
+
+        list.add("Select Language(अपनी भाषा चुने)");
+        list.add("English");
+        list.add("Hindi(हिन्दी)");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+    spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+
+    {
+        @Override
+        public void onItemSelected (AdapterView< ? > adapterView, View view, int position, long l){
+        switch (position) {
+            case 0:
+                break;
+            case 1:
+                setLocale("en");
+                break;
+            case 2:
+                setLocale("hi");
+                break;
+        }
+    }
+        @Override
+        public void onNothingSelected (AdapterView < ? > adapterView){
+    }
+    });
+}
+
+    private void setLocale(String localName) {
+        if (!localName.equals(currentLanguage)) {
+            myLocale = new Locale(localName);
+            Resources res = getResources();
+            DisplayMetrics dm = res.getDisplayMetrics();
+            Configuration conf = res.getConfiguration();
+            conf.locale = myLocale;
+            res.updateConfiguration(conf, dm);
+            Intent refresh = new Intent(this, LangActivity.class);
+            refresh.putExtra(currentLang, localName);
+            startActivity(refresh);
+        } else {
+            Toast.makeText(LangActivity.this, "Language already selected!", Toast.LENGTH_SHORT).show();
+        }
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        onBackPressed();
-        return true;
-    }
-
-    public void com(View view){
-        Intent intent = new Intent(this,MainActivity.class);
-        startActivity(intent);
-
-    }
 
     @Override
     public void onBackPressed() {
-        AlertDialog.Builder b = new AlertDialog.Builder(LangActivity.this);
-        b.setMessage("ARE YOU SURE YOU WANT TO EXIT?").setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                LangActivity.super.onBackPressed();
-            }
-        }).setNegativeButton("Cancel",null).setCancelable(false);
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        finish();
+        System.exit(0);
+    }
 
-        AlertDialog alert = b.create();
-        alert.show();
+    public void com(View view){
+        Intent i = new Intent(this,MainActivity.class);
+        startActivity(i);
     }
 }
